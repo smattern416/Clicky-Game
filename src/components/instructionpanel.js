1f528dbc98be30117633eb1408898f9cc0df969d
Stephanie Mattern <smattern416@gmail.com>
2019-07-29 08:12:52 -0400

    created initial files and downloaded required

---
+++ src/components/instructionpanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const InstructionPanel = () => {
+  return (
+    <div>
+      <h1>Clicky Game!</h1>
+      <h5>
+        Click on an image to earn points- but make sure you don't click on it more than once!
+      </h5>
+    </div>
+  );
+};
+export default InstructionPanel;
