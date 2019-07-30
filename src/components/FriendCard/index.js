d59506a56050d31279ccb61b0803c3ccfea50fe9
Stephanie Mattern <smattern416@gmail.com>
2019-07-30 17:51:57 -0400

    updated index.js

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -12,12 +12,6 @@ function FriendCard(props) {
           <li>
             <strong>Name:</strong> {props.name}
           </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
         </ul>
       </div>
       <span onClick={() => props.removeFriend(props.id)} className="remove">
